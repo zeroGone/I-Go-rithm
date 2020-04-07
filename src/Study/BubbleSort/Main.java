c4b0b6d78ab885dae925b0292dd91c97b143901c
YoungGon KIM <33260476+zerogone@users.noreply.github.com>
2020-04-07 12:51:31 +0900

    BubbleSort, Insertion Sort

---
+++ src/Study/BubbleSort/Main.java
@@ -0,0 +1,36 @@
+package Study.BubbleSort;
+
+import java.util.Arrays;
+
+/*
+ * 버블정렬
+ * 가장 큰 수를 옮기는 방법이 다를 뿐 선택정렬과 비슷하다
+ * N개의 원소를 가진 배열에서 이웃하는 원소들을 비교하여 바꾸어 나간다
+ * 수행시간은 O(N^2)
+ * */
+public class Main {
+
+    static int[] array = {9, 1, 6, 8, 4, 3, 2, 0};
+
+
+    public static void main(String[] args) {
+        bubbleSort();
+        System.out.println(Arrays.toString(array));
+    }
+
+    private static void bubbleSort() {
+        for (int i = array.length-1; i>=0; i--){
+            boolean sorted = true; // 정렬이 되어있을 때 불필요한 반복을 검사
+            for(int j = 0; j<i; j++){
+                if(array[j]>array[j+1]){
+                    //swap
+                    int temp = array[j+1];
+                    array[j+1] = array[j];
+                    array[j] = temp;
+                    sorted = false;
+                }
+            }
+            if(sorted) break;
+        }
+    }
+}
---
+++ src/Study/InsertionSort/Main.java
@@ -0,0 +1,34 @@
+package Study.InsertionSort;
+
+import java.util.Arrays;
+
+/*
+삽입정렬
+이미 정렬된 i개의 원소를 가진 배열에서 하나의 원소를 더하여 i+1개의 배열을 만드는 과정 반복
+i+1번째의 원소를 i개의 원소를 가진 정렬된 배열에서 적당한 위치로 끼워넣음
+버블정렬과 선택정렬은 N개의 배열에서 시작하지만
+삽입정렬은 1개의 배열부터 시작
+수행시간은 O(N^2)
+*/
+public class Main {
+
+    static int[] array = {9, 1, 6, 8, 4, 3, 2, 0};
+
+    public static void main(String[] args) {
+        insertionSort();
+    }
+
+    static void insertionSort() {
+        for (int i = 1; i < array.length; i++) {
+            int loc = i - 1;
+            int newItem = array[i];
+            while (loc >= 0 && newItem < array[loc]) {
+                array[loc + 1] = array[loc];
+                loc--;
+            }
+
+            array[loc + 1] = newItem;
+            System.out.println(Arrays.toString(array));
+        }
+    }
+}
