fca2b6fc7cc2b135c055d4e1f3c5bcbec907c482
YoungGon KIM <33260476+zeroGone@users.noreply.github.com>
2020-04-07 09:02:13 +0900

    initial commit

---
+++ src/Study/SelectSort/Main.java
@@ -0,0 +1,31 @@
+package Study.SelectSort;
+
+import java.util.Arrays;
+
+/*
+선택정렬
+1. N 개의 원소를 가진 배열에서 가장 큰 원소를 찾는다
+2. 가장 마지막 인덱스의 원소와 자리를 바꾼다
+3. 가장 큰 원소는 자기 자리를 찾았으므로 신경 쓰지 않는다
+4. 나머지 원소들로 같은 작업을 반복
+
+수행 시간 : 모든 경우 O(n^2)
+*/
+public class Main {
+    public static void main(String[] args) {
+        int[] array = {9, 1, 6, 8, 4, 3, 2, 0};
+
+        for(int i=array.length-1; i>=0; i--){
+            int maxIndex = i;
+            for(int j=0; j<i; j++){
+                if(array[maxIndex]<array[j]) maxIndex=j;
+            }
+
+            int temp = array[maxIndex];
+            array[maxIndex] = array[i];
+            array[i] = temp;
+
+            System.out.println(Arrays.toString(array));
+        }
+    }
+}
---
+++ src/baekjoon/정렬/나이순정렬.java
@@ -0,0 +1,49 @@
+package baekjoon.정렬;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class 나이순정렬 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(reader.readLine());
+
+        Member[] members = new Member[N];
+
+        for (int i = 0; i < N; i++) {
+            String[] temp = reader.readLine().split(" ");
+            members[i] = new Member(Integer.parseInt(temp[0]), temp[1]);
+        }
+
+        Arrays.sort(members);
+
+        StringBuilder builder = new StringBuilder();
+        for (Member m : members) {
+            builder.append(m);
+            builder.append('\n');
+        }
+        System.out.println(builder.toString());
+    }
+
+    static class Member implements Comparable<Member> {
+        int age;
+        String name;
+
+        Member(int age, String name) {
+            this.age = age;
+            this.name = name;
+        }
+
+        @Override
+        public int compareTo(Member o) {
+            return this.age-o.age;
+        }
+
+        @Override
+        public String toString() {
+            return age + " " + name;
+        }
+    }
+}
