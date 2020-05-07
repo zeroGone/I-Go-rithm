0e7a34b391ef4e08a09c7e8bf66f3ee37b1d0fb7
YoungGon KIM <33260476+zerogone@users.noreply.github.com>
2020-05-07 20:34:13 +0900

    빽

---
+++ src/baekjoon/백트래킹/N과M2.java
@@ -0,0 +1,34 @@
+package baekjoon.백트래킹;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.Scanner;
+
+public class N과M2 {
+    static BufferedWriter writer;
+    static int N;
+    static int M;
+
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        N = scanner.nextInt();
+        M = scanner.nextInt();
+        writer = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        for (int i = 0; i < N; i++) {
+            solution(1, i , (i+1)+" ");
+        }
+        writer.flush();
+        writer.close();
+    }
+
+    private static void solution(int num, int value, String output) throws IOException {
+        if (num == M) writer.write(output.substring(0, output.length() - 1) + '\n');
+        if (num >= M) return;
+
+        for (int i = value+1; i < N; i++) {
+            solution(num+1, i,output + (i + 1) + " ");
+        }
+    }
+}
