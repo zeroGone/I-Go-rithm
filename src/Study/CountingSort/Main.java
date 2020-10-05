d5a739df72ffaf98436adcf789518417826f7982
YoungGon KIM <33260476+zerogone@users.noreply.github.com>
2020-10-06 00:16:14 +0900

    1006

---
+++ src/Study/CountingSort/Main.java
@@ -0,0 +1,27 @@
+package Study.CountingSort;
+
+import java.util.*;
+/*
+* 값들의 개수를 셀 때 주로 쓰임. 값의 종류가 많아질 수록 비효율적
+* */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(countingSort("hello world. good morning. how are you."));
+    }
+
+    private static String countingSort(String s){
+        Map<Character, Integer> map = new TreeMap<>();
+        for(char c : s.toCharArray()){
+            int num = 0;
+            if(map.containsKey(c)) num = map.get(c);
+            map.put(c, num+1);
+        }
+
+        StringBuilder builder = new StringBuilder();
+        for(char c : map.keySet()){
+            int num = map.get(c);
+            for(int i=0; i<num; i++) builder.append(c);
+        }
+        return builder.toString();
+    }
+}
---
+++ src/Study/QuickSort/Main.java
@@ -0,0 +1,48 @@
+package Study.QuickSort;
+
+import java.util.Arrays;
+import java.util.Random;
+
+/*
+ * 퀵정렬
+ * 1. 배열에서 하나의 원소를 고른다. 선택된 원소를 Pivot 이라 한다.
+ * 2. Pivot을 기준으로 Pivot 보다 작은 영역과 큰 영역을 분할하고 정렬한다.
+ * 3. 정렬된 두 영역에서 피벗에 넣고 재귀로 반복한다.
+ * 평균 수행시간 : O(NlogN), 최악 : O(N^2)
+ * */
+
+public class Main {
+    public static void main(String[] args) {
+        Random random = new Random();
+        int[] array = new int[10];
+        for (int i = 0; i < array.length; ++i) array[i] = random.nextInt(20);
+        System.out.println(Arrays.toString(array));
+        quickSort(array, 0, array.length - 1);
+        System.out.println(Arrays.toString(array));
+    }
+
+    static void quickSort(int[] array, int start, int end) {
+        if (start < end) {
+            int middle = partition(array, start, end);
+            quickSort(array, start, middle - 1);
+            quickSort(array, middle + 1, end);
+        }
+    }
+
+    static int partition(int[] array, int start, int end) {
+        int pivot = array[end];
+        int index1 = start - 1;
+
+        for (int index2 = start; index2 < end; index2++) {
+            if (array[index2] <= pivot) swap(array, ++index1, index2);
+        }
+        swap(array, ++index1, end);
+        return index1;
+    }
+
+    static void swap(int[] array, int i, int j) {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+}
