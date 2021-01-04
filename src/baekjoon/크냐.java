c5bf7efaed612e8c559bd3c604fbef22143d20ef
YoungGon KIM <33260476+zeroGone@users.noreply.github.com>
2021-01-04 16:21:42 +0900

    0104

--- src/baekjoon/크냐.java
+++ src/baekjoon/크냐.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main {
+public class 크냐 {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
