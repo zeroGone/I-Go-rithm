462c99355e0e7840649c6f55fb937e9eddeef9b9
YoungGon KIM <33260476+zeroGone@users.noreply.github.com>
2021-01-16 20:28:35 +0900

    0116

--- src/programmers/입국심사.java
+++ src/programmers/입국심사.java
@@ -1,5 +1,6 @@
 package programmers;
 
+import java.util.Arrays;
 
 public class 입국심사 {
     public static void main(String[] args) {
@@ -9,61 +10,36 @@ public class 입국심사 {
 //        }
 //        System.out.println(new 입국심사().solution(1000000000, a));
 
-        System.out.println(new 입국심사().solution(6, new int[]{7,10}));
-//        System.out.println(new 입국심사().solution(10, new int[]{2,3,4,5}));
+        System.out.println(new 입국심사().solution(6, new int[]{7, 10}));
+        System.out.println(new 입국심사().solution(10, new int[]{2, 3, 4, 5}));
+        System.out.println(new 입국심사().solution(10, new int[]{1, 5}));
+        System.out.println(new 입국심사().solution(1, new int[]{2, 2}));
     }
 
     public long solution(int n, int[] times) {
-        long answer = 0;
+        Arrays.sort(times);
+        long start = 0;
+        long end = Long.MAX_VALUE;
+        long answer = Long.MAX_VALUE;
 
-        boolean[] check = new boolean[n];
+        while (start <= end) {
+            long middle = (start + end) / 2;
 
-        int pre = 0;
-        check[pre] = true;
-        answer+=times[pre];
-        for (int i = 0; i < n; i++) {
-            int min = -1;
-            for (int j = 0; j < times.length; j++) {
-                if(!check[j]){
-                    if(min == -1 || times[min] > times[j]) min = j;
-                }
-            }
-            int examineMin = Integer.MAX_VALUE;
-            int examineMinIndex = -1;
-            for (int j = 0; j < times.length; j++) {
-                if(check[j]){
-                    if(times[j] < examineMin){
-                        examineMin = times[j];
-                        examineMinIndex = j;
-                    }
+            long peopleNumber = 0;
+            for (int time : times) {
+                peopleNumber += middle / time;
+
+                if (peopleNumber >= n) {
+                    break;
                 }
             }
 
-            int value = 0;
-            int index = 0;
-
-            if(examineMinIndex == -1){
-                value = times[min];
-                index = min;
-            }else if(min == -1){
-                value = examineMin;
-                index = examineMinIndex;
-            }else{
-                if(times[min] < examineMin){
-                    value = times[min];
-                    index = min;
-                }else{
-                    value = examineMin;
-                    index = examineMinIndex;
-                }
+            if (peopleNumber < n) {
+                start = middle + 1;
+            } else {
+                answer = Math.min(answer, middle);
+                end = middle - 1;
             }
-
-            System.out.println(value + " " +index);
-
-            answer+=value;
-            check[pre] = false;
-            check[index] = true;
-            pre = index;
         }
 
         return answer;
